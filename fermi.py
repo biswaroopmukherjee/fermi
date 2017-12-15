ccae4460c0cf0f0b21259e5d7f8d86b388636f37
Biswaroop Mukherjee <mail.biswaroop@gmail.com>
2017-12-15 07:41:06 +0000

    So many changes to get it to work on the pi. No overlapping streams. Still needs a bunch of refactoring, but the basic idea seems to work. Need a better soundcard.

--- fermi.py
+++ fermi.py
@@ -52,8 +52,19 @@ DIALOG_FOLLOW_ON = embedded_assistant_pb2.ConverseResult.DIALOG_FOLLOW_ON
 CLOSE_MICROPHONE = embedded_assistant_pb2.ConverseResult.CLOSE_MICROPHONE
 DEFAULT_GRPC_DEADLINE = 60 * 3 + 5
 
+
+api_endpoint=ASSISTANT_API_ENDPOINT
+credentials = '/home/pi/.config/google-oauthlib-tool/credentials.json'
+verbose = True
+grpc_deadline = DEFAULT_GRPC_DEADLINE
+model = 'resources/Fermi 3.pmdl'
+
+
 interrupted = False
 start_stop = True
+
+detector = None
+assistant = None
 
 ser = None
 serio = None
@@ -120,9 +131,10 @@ def keysend(letter, modifier=None):
 
 
 # NLP preprocessor for lab related stuff. If this fails, fall back on google
-def labwork(text):
+def labwork(text, quiet):
     ''' Very hacky NLP on the text blob '''
     playstandard = False
+    quietout = quiet
     logger = logging.getLogger("fermi")
     text = text.lower()
     words = re.split(' ', text)
@@ -131,104 +143,107 @@ def labwork(text):
     if 'trap' in words or ('pumping' not in words and 'imaging' not in words and ('sodium' in words or 'lithium' in words)):
         if 'lithium' in words:
             logger.info('CTRL-L')
-            speaktext('Turning on lithium mott')
+            if not quiet: speaktext('Turning on lithium mott')
             keysend('L', 'Ctrl')
         elif 'sodium' in words:
-            speaktext('Turning on sodium mott')
+            if not quiet: speaktext('Turning on sodium mott')
             logger.info('CTRL-N')
             keysend('N', 'Ctrl')
         else:
-            speaktext('Please specify the atom.')
+            if not quiet: speaktext('Please specify the atom.')
              # need to make it more conversational: Sodium or lithium? Context needed
             logger.info('unknown atom')
     elif 'pumping' in words:
         if 'lithium' in words:
-            speaktext('Turning on lithium pumping')
+            if not quiet: speaktext('Turning on lithium pumping')
             logger.info('CTRL-P')
             keysend('P', 'Ctrl')
         elif 'sodium' in words:
-            speaktext('Turning on sodium pumping')
+            if not quiet: speaktext('Turning on sodium pumping')
             logger.info('CTRL-Q')
             keysend('Q', 'Ctrl')
         else:
-            speaktext('Please specify the atom')
+            if not quiet: speaktext('Please specify the atom')
             logger.info('unknown atom')
     elif 'imaging' in words:
-        speaktext('Turning on imaging')
+        if not quiet: speaktext('Turning on imaging')
         logger.info('CTRL-I')
         keysend('I', 'Ctrl')
     elif 'abort' in words:
-        speaktext('Aborting')
+        if not quiet: speaktext('Aborting')
         logger.info('esc')
         keysend('escape')
     elif 'run' in words or 'running' in words and 'trap' not in words:
-        if 'list' in words:
-            speaktext('Running list')
+        if 'list' in words or 'twelve' in words:
+            if not quiet: speaktext('Running list')
             logger.info('F12')
             keysend('F12')
         elif 'background' in words:
-            speaktext('Running in background')
+            if not quiet: speaktext('Running in background')
             logger.info('CTRL-F9')
             keysend('F9')
         else:
-            speaktext('Running sequence')
+            if not quiet: speaktext('Running sequence')
             logger.info('F9')
             keysend('F9')
     elif 'stop' in words or 'control' in words or 'nothing' in words:
-        speaktext('Okay. Doing nothing.')
+        if not quiet: speaktext('Okay. Doing nothing.')
         logger.info('CTRL-Z')
         keysend('Z', 'Ctrl')
     elif 'lights' in words or 'room' in words:
         try:
             if 'off' in words:
-                speaktext('Turning off room lights.')
+                if not quiet: speaktext('Turning off room lights.')
                 switch.turn_off()
             else:
-                speaktext('Turning on room lights.')
+                if not quiet: speaktext('Turning on room lights.')
                 switch.turn_on()
         except:
-            speaktext("I'm sorry. I can't connect to the light switch right now.")
-            logger.error('Cant talk to the switchmate')
+            logger.debug('well I need to fix this error')
     elif 'quiet' in words or 'shut' in words:
-        quiet = True
+        quietout = True
         logger.info('turning off text to speech')
     elif 'stupid' in words:
         logger.info('theyre being mean')
-        speaktext("I'm doing my best. Please try not to be mean to me.")
+        if not quiet: speaktext("I'm doing my best. Please try not to be mean to me.")
     elif 'speak' in words or 'speaking' in words:
-        quiet = False
+        quietout = False
         speaktext("Thank you. I've been dying to talk.")
-        logger.info('turning off text to speech')
+        logger.info('turning on text to speech')
     elif 'name' in words:
-        speaktext('why')
+        if not quiet: speaktext('why')
         logger.info('why')
     elif 'why' in words:
-        speaktext('Because you told me so.')
+        if not quiet: speaktext('Because you told me so.')
         logger.info('Classic matlab')
     elif 'igor' in words:
-        speaktext('Igor? I prefer python.')
+        if not quiet: speaktext('Igor? I prefer python.')
     elif 'morning' in words:
-        speaktext('Good morning!')
+        if not quiet: speaktext('Good morning!')
     elif 'papers' in words or 'paper' in words or 'archive' in words:
         logger.info('checking the arxiv')
-        speaktext("Hold on while I check the archive.")
+        if not quiet: speaktext("Hold on while I check the archive.")
         reader = Reader(detailed=True)
         try:
             reader.download_info()
         except:
-            speaktext("Sorry. I couldn't connect to the archive")
+            if not quiet: speaktext("Sorry. I couldn't connect to the archive")
             logger.error('Cant connect or download the interest list')
         try:
             text_to_say = reader.read_arxiv()
-            speaktext(text_to_say)
+            if not quiet: speaktext(text_to_say)
+            logger.info(text_to_say)
         except:
-            speaktext("I'm sorry. I can't do that right now.")
+            if not quiet: speaktext("I'm sorry. I can't do that right now.")
             logger.error('Cant read the arxiv')
+    elif 'pod' in words or 'bay' in words or 'door' in words:
+        if not quiet: speaktext("I'm sorry Dave. I'm afraid I can't do that")
+        logger.info('Hal')
     else:
         # Run standard google response
         # speaktext("Sorry I didn't understand that.")
         playstandard = True
-    return playstandard
+    return playstandard, quietout
 
 
 
@@ -243,8 +258,8 @@ class FermiAssistant(object):
       deadline_sec: gRPC deadline in seconds for Google Assistant API call.
     """
 
-    def __init__(self, conversation_stream, channel, deadline_sec):
-        self.conversation_stream = conversation_stream
+    def __init__(self, channel, deadline_sec):
+        
 
         # Opaque blob provided in ConverseResponse that,
         # when provided in a follow-up ConverseRequest,
@@ -257,8 +272,8 @@ class FermiAssistant(object):
         # Create Google Assistant API gRPC client.
         self.assistant = embedded_assistant_pb2.EmbeddedAssistantStub(channel)
         self.deadline = deadline_sec
-
         self.logger = logging.getLogger("fermi")
+        self.quiet = False
 
     def __enter__(self):
         return self
@@ -284,6 +299,36 @@ class FermiAssistant(object):
         """
         continue_conversation = False
 
+        # Configure audio source and sink.
+        audio_device = None
+
+        audio_source = audio_device = (
+            audio_device or audio_helpers.SoundDeviceStream(
+                sample_rate=16000,
+                sample_width=2,
+                block_size=6400,
+                flush_size=25600
+            )
+        )
+
+        audio_sink = audio_device = (
+            audio_device or audio_helpers.SoundDeviceStream(
+                sample_rate=16000,
+                sample_width=2,
+                block_size=6400,
+                flush_size=25600
+            )
+        )
+        # Create conversation stream with the given audio source and sink.
+        conversation_stream = audio_helpers.ConversationStream(
+            source=audio_source,
+            sink=audio_sink,
+            iter_size=3200,
+            sample_width=2,
+        )
+        
+        self.conversation_stream = conversation_stream
+
         self.conversation_stream.start_recording()
         self.logger.info('Recording audio request.')
 
@@ -308,12 +353,12 @@ class FermiAssistant(object):
             if resp.result.spoken_request_text:
                 self.logger.info('Transcript of user request: "%s".',
                              resp.result.spoken_request_text)
-                playstandard = labwork(resp.result.spoken_request_text)
+                playstandard, self.quiet = labwork(resp.result.spoken_request_text, self.quiet)
                 # playstandard = True
                 if playstandard:
                     self.logger.info('Playing assistant response.')
 
-            if len(resp.audio_out.audio_data) > 0 and playstandard:
+            if len(resp.audio_out.audio_data) > 0 and playstandard and not self.quiet:
                 self.conversation_stream.write(resp.audio_out.audio_data)
             # if resp.result.spoken_response_text:
             #     self.logger.info(
@@ -332,8 +377,11 @@ class FermiAssistant(object):
             elif resp.result.microphone_mode == CLOSE_MICROPHONE:
                 continue_conversation = False
         self.logger.info('Finished response.')
-
         self.conversation_stream.stop_playback()
+        try:
+            self.conversation_stream.close()
+        except:
+            self.logger.error("Cant close conversation stream")
         return continue_conversation
 
     def gen_converse_requests(self):
@@ -365,155 +413,60 @@ class FermiAssistant(object):
             # Subsequent requests need audio data, but not config.
             yield embedded_assistant_pb2.ConverseRequest(audio_in=data)
 
-def testcallback():
+def testCallback():
     logger = logging.getLogger("fermi")
     logger.info('Hi! Speak friend.')
 
-
-# Click options for fermi.py
-@click.command()
-@click.option('--api-endpoint', default=ASSISTANT_API_ENDPOINT,
-              metavar='<api endpoint>', show_default=True,
-              help='Address of Google Assistant API service.')
-@click.option('--credentials',
-              metavar='<credentials>', show_default=True,
-              default='/home/pi/.config/google-oauthlib-tool/credentials.json',
-              help='Path to read OAuth2 credentials.')
-@click.option('--verbose', '-v', is_flag=True, default=False,
-              help='Verbose logger.')
-@click.option('--input-audio-file', '-i',
-              metavar='<input file>',
-              help='Path to input audio file. '
-              'If missing, uses audio capture')
-@click.option('--output-audio-file', '-o',
-              metavar='<output file>',
-              help='Path to output audio file. '
-              'If missing, uses audio playback')
-@click.option('--audio-sample-rate',
-              default=audio_helpers.DEFAULT_AUDIO_SAMPLE_RATE,
-              metavar='<audio sample rate>', show_default=True,
-              help='Audio sample rate in hertz.')
-@click.option('--audio-sample-width',
-              default=audio_helpers.DEFAULT_AUDIO_SAMPLE_WIDTH,
-              metavar='<audio sample width>', show_default=True,
-              help='Audio sample width in bytes.')
-@click.option('--audio-iter-size',
-              default=audio_helpers.DEFAULT_AUDIO_ITER_SIZE,
-              metavar='<audio iter size>', show_default=True,
-              help='Size of each read during audio stream iteration in bytes.')
-@click.option('--audio-block-size',
-              default=audio_helpers.DEFAULT_AUDIO_DEVICE_BLOCK_SIZE,
-              metavar='<audio block size>', show_default=True,
-              help=('Block size in bytes for each audio device '
-                    'read and write operation..'))
-@click.option('--audio-flush-size',
-              default=audio_helpers.DEFAULT_AUDIO_DEVICE_FLUSH_SIZE,
-              metavar='<audio flush size>', show_default=True,
-              help=('Size of silence data in bytes written '
-                    'during flush operation'))
-@click.option('--grpc-deadline', default=DEFAULT_GRPC_DEADLINE,
-              metavar='<grpc deadline>', show_default=True,
-              help='gRPC deadline in seconds')
-@click.option('--model', default='resources/Fermi 3.pmdl',
-        metavar='<snowboy model>', show_default=True,
-        help='Snowboy model for hotword')
-
-
-# Main function.
-
-def main(api_endpoint, credentials, verbose,
-         audio_sample_rate, audio_sample_width,
-         audio_iter_size, audio_block_size, audio_flush_size,
-         grpc_deadline, model, *args, **kwargs):
-
-    """Google assistant main function
-    """
-    #
-    # Setup logger.
-    logging.basicConfig()
-    logger = logging.getLogger("fermi")
-    if verbose:
-        logger.setLevel(logging.DEBUG)
-    else:
-        logger.setLevel(logging.INFO)
-
-    signal.signal(signal.SIGINT, signal_handler)
-
-    # Setup the serial connection to the keyboard
-    ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, rtscts=True)
-    serio = io.TextIOWrapper(io.BufferedRWPair(ser,ser,1), newline='\r\n', line_buffering=True)
-
-    # Setup the switchmate
-    switch = switchmate.Switch()
-
-    # Start listening with snowboy
-    detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
-    print('Listening... Press Ctrl+C to exit')
-
-    # Load OAuth 2.0 credentials.
-    try:
-        with open(credentials, 'r') as f:
-            credentials = google.oauth2.credentials.Credentials(token=None,
-                                                                **json.load(f))
-            http_request = google.auth.transport.requests.Request()
-            credentials.refresh(http_request)
-    except Exception as e:
-        logger.error('Error loading credentials: %s', e)
-        logger.error('Run google-oauthlib-tool to initialize '
-                      'new OAuth 2.0 credentials.')
-        return
-
-    # Create an authorized gRPC channel.
-    grpc_channel = google.auth.transport.grpc.secure_authorized_channel(
-        credentials, http_request, api_endpoint)
-    logger.info('Connecting to %s', api_endpoint)
-
-    # Configure audio source and sink.
-    audio_device = None
-
-    audio_source = audio_device = (
-        audio_device or audio_helpers.SoundDeviceStream(
-            sample_rate=audio_sample_rate,
-            sample_width=audio_sample_width,
-            block_size=audio_block_size,
-            flush_size=audio_flush_size
-        )
-    )
-
-    audio_sink = audio_device = (
-        audio_device or audio_helpers.SoundDeviceStream(
-            sample_rate=audio_sample_rate,
-            sample_width=audio_sample_width,
-            block_size=audio_block_size,
-            flush_size=audio_flush_size
-        )
-    )
-    # Create conversation stream with the given audio source and sink.
-    conversation_stream = audio_helpers.ConversationStream(
-        source=audio_source,
-        sink=audio_sink,
-        iter_size=audio_iter_size,
-        sample_width=audio_sample_width,
-    )
-
-    with FermiAssistant(conversation_stream, grpc_channel, grpc_deadline) as assistant:
-        # main loop
-        #detector.start(detected_callback=assistant.converse,
-                    #interrupt_check=interrupt_callback,
-                    #sleep_time=0.03)
-
-        detector.start(detected_callback=testcallback, interrupt_check=interrupt_callback, sleep_time=0.03)
-
-    conversation_stream.close()
-
+def fermiCallback():
     detector.terminate()
-    #
-    # detect = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
-    # detect.start(detected_callback=[lambda: main()],
-    #                   interrupt_check=interrupt_callback,
-    #                   sleep_time=0.05)
+    assistant.converse()
+    detector.start(detected_callback=callback_to_use, interrupt_check=interrupt_callback,sleep_time=0.03)
 
-if __name__ == "__main__":
-    main()
 
-# detector.terminate()
+# Setup logger.
+logging.basicConfig()
+logger = logging.getLogger("fermi")
+if verbose:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+# Setup the serial connection to the keyboard
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200, rtscts=True)
+serio = io.TextIOWrapper(io.BufferedRWPair(ser,ser,1), newline='\r\n', line_buffering=True)
+
+# Setup the switchmate
+switch = switchmate.Switch()
+
+# Start listening with snowboy
+detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+print('Listening... Press Ctrl+C to exit')
+
+# Load OAuth 2.0 credentials.
+try:
+    with open(credentials, 'r') as f:
+        credentials = google.oauth2.credentials.Credentials(token=None,
+                                                            **json.load(f))
+        http_request = google.auth.transport.requests.Request()
+        credentials.refresh(http_request)
+except Exception as e:
+    logger.error('Error loading credentials: %s', e)
+    logger.error('Run google-oauthlib-tool to initialize '
+                  'new OAuth 2.0 credentials.')
+
+# Create an authorized gRPC channel.
+grpc_channel = google.auth.transport.grpc.secure_authorized_channel(
+    credentials, http_request, api_endpoint)
+logger.info('Connecting to %s', api_endpoint)
+
+# Make the assistant and callback
+assistant = FermiAssistant(grpc_channel, grpc_deadline)
+callback_to_use = fermiCallback
+
+# main loop
+detector.start(detected_callback=callback_to_use, interrupt_check=interrupt_callback,sleep_time=0.03)
+
+detector.terminate()
+
